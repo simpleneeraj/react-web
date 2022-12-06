602b337fe615ac413f5cb1367675bc0753779214
simpleneeraj <teamsimpleneeraj@gmail.com>
2022-12-06 08:50:35 +0530

    my first react app

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+const CounterApp = () => {
+  return (
+    <div className="">
+      <h1>Counter App</h1>
+    </div>
+  );
+};
+
+export default CounterApp;
+// export { CounterApp };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import CounterApp from "./components/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const RootComponent = () => {
+  return (
+    <div>
+      <CounterApp />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
